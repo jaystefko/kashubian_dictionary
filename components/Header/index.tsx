8e5c605c93177962d181e301dae6342de7f10fd0
JakubStefko <jakub.stefko@gmail.com>
2022-08-22 22:06:10 +0200

    and another one

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,6 +1,7 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import styles from './styles.module.css'
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './styles.module.css';
+import { Facebook, Instagram, QuestionMark, Message } from '@mui/icons-material';
 
 function Header() {
   return (
@@ -26,27 +27,41 @@ function Header() {
         </ul>
         <ul className={styles.iconList}>
           <li>
-            <a className={styles.icon} href='https://www.facebook.com/Sloworz' rel='external nofollow'>
-              F
+            <a
+              className={styles.icon}
+              href='https://www.facebook.com/Sloworz'
+              rel='external nofollow'
+            >
+              <Facebook />
             </a>
           </li>
           <li>
-            <a className={styles.icon} href='https://twitter.com/kaszubskieslowa' rel='external nofollow'>
-              T
+            <a
+              className={styles.icon}
+              href='https://www.instagram.com/polsko_kaszebsczi_sloworz'
+              rel='external nofollow'
+            >
+              <Instagram />
             </a>
           </li>
           <li>
-            <a className={styles.icon} href='https://www.facebook.com/Sloworz' rel='external nofollow'>
-              I
+            <a
+              className={styles.icon}
+              href='https://twitter.com/kaszubskieslowa'
+              rel='external nofollow'
+            >
+              <Message />
             </a>
           </li>
         </ul>
         <Link href='/howTo'>
-          <a className={styles.icon}>?</a>
+          <a className={styles.icon}>
+            <QuestionMark />
+          </a>
         </Link>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,8 +1,22 @@
-import type { NextPage } from 'next'
-import LoginBox from '../../components/LoginBox'
+import type { NextPage } from 'next';
+import { useEffect } from 'react';
+import LoginBox from '../../components/LoginBox';
 
 const AdminScreen: NextPage = () => {
-  return <LoginBox />
-}
+  useEffect(() => {
+    (async () => {
+      let auth = localStorage.getItem('auth');
+      if (!auth) {
+        const username = (await prompt('Fill in your login', '')) || '';
+        console.log(username);
+        const password = (await prompt('Fill in your password', '')) || '';
+        console.log(password);
+        localStorage.setItem('auth', JSON.stringify({ username, password }));
+      }
+    })();
+  }, []); // eslint-disable-line
 
-export default AdminScreen
+  return <LoginBox />;
+};
+
+export default AdminScreen;
