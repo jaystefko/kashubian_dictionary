c867fe9ebb891fb8c023410ff1f14045df745d8e
JakubStefko <jakub.stefko@gmail.com>
2022-09-16 10:24:48 +0200

    header menu translation and titles

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,27 +2,37 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from './styles.module.css';
 import { Facebook, Instagram, QuestionMark, Message } from '@mui/icons-material';
+import { useIntl } from 'react-intl';
 
 function Header() {
+  const intl = useIntl();
+
   return (
     <header className={styles.header}>
       <Link href='/'>
         {/* 453px width; 172px height in original image */}
-        <Image src='/images/logo.png' height={90} width={237} alt='Logo' role='link' />
+        <Image
+          src='/images/logo.png'
+          height={90}
+          width={237}
+          alt='Logo'
+          role='link'
+          title={intl.formatMessage({ id: 'topMenu.startPage' })}
+        />
       </Link>
       <div>
         <ul className={styles.navList}>
           <li>
-            <Link href='/about'>O słowniku</Link>
+            <Link href='/about'>{intl.formatMessage({ id: 'topMenu.about' })}</Link>
           </li>
           <li>
-            <Link href='/foundation'>Fundacja Kaszuby</Link>
+            <Link href='/foundation'>{intl.formatMessage({ id: 'topMenu.foundation' })}</Link>
           </li>
           <li>
-            <Link href='/lessons'>Lekcje</Link>
+            <Link href='/lessons'>{intl.formatMessage({ id: 'topMenu.lessons' })}</Link>
           </li>
           <li>
-            <Link href='/comments'>Zgłoś uwagi</Link>
+            <Link href='/comments'>{intl.formatMessage({ id: 'topMenu.comments' })}</Link>
           </li>
         </ul>
         <ul className={styles.iconList}>
@@ -30,7 +40,9 @@ function Header() {
             <a
               className={styles.icon}
               href='https://www.facebook.com/Sloworz'
-              rel='external nofollow'
+              rel='external nofollow noreferrer'
+              target='_blank'
+              title={intl.formatMessage({ id: 'topMenu.fb' })}
             >
               <Facebook />
             </a>
@@ -39,7 +51,9 @@ function Header() {
             <a
               className={styles.icon}
               href='https://www.instagram.com/polsko_kaszebsczi_sloworz'
-              rel='external nofollow'
+              rel='external nofollow noreferrer'
+              target='_blank'
+              title={intl.formatMessage({ id: 'topMenu.instagram' })}
             >
               <Instagram />
             </a>
@@ -48,14 +62,16 @@ function Header() {
             <a
               className={styles.icon}
               href='https://twitter.com/kaszubskieslowa'
-              rel='external nofollow'
+              rel='external nofollow noreferrer'
+              target='_blank'
+              title={intl.formatMessage({ id: 'topMenu.tweeter' })}
             >
               <Message />
             </a>
           </li>
         </ul>
         <Link href='/howTo'>
-          <a className={styles.icon}>
+          <a className={styles.icon} title={intl.formatMessage({ id: 'topMenu.howTo' })}>
             <QuestionMark />
           </a>
         </Link>
