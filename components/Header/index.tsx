081b7342d3666433b71ee9627c6e8efc4485be95
JakubStefko <jakub.stefko@gmail.com>
2022-09-26 00:49:00 +0200

    ...

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,21 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './styles.module.css';
-import { Facebook, Instagram, QuestionMark, Message } from '@mui/icons-material';
+import { Facebook, Instagram, QuestionMark, Message, Menu as ManuIcon } from '@mui/icons-material';
+import { Button, Menu, MenuItem } from '@mui/material';
 import { useIntl } from 'react-intl';
+import { MouseEvent, useState } from 'react';
 
 function Header() {
   const intl = useIntl();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <header className={styles.header}>
@@ -20,7 +30,7 @@ function Header() {
           title={intl.formatMessage({ id: 'topMenu.startPage' })}
         />
       </Link>
-      <div>
+      <div className={styles.desktopContainer}>
         <ul className={styles.navList}>
           <li>
             <Link href='/about'>{intl.formatMessage({ id: 'topMenu.about' })}</Link>
@@ -69,12 +79,74 @@ function Header() {
               <Message />
             </a>
           </li>
+          <li>
+            <Link href='/howTo'>
+              <a className={styles.icon} title={intl.formatMessage({ id: 'topMenu.howTo' })}>
+                <QuestionMark />
+              </a>
+            </Link>
+          </li>
         </ul>
-        <Link href='/howTo'>
-          <a className={styles.icon} title={intl.formatMessage({ id: 'topMenu.howTo' })}>
-            <QuestionMark />
-          </a>
-        </Link>
+      </div>
+      <div className={styles.mobileContainer}>
+        <Button
+          id='basic-button'
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup='true'
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+        >
+          <ManuIcon />
+        </Button>
+        <Menu
+          id='basic-menu'
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <MenuItem onClick={handleClose}>
+            <Link href='/about'>{intl.formatMessage({ id: 'topMenu.about' })}</Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link href='/foundation'>{intl.formatMessage({ id: 'topMenu.foundation' })}</Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link href='/lessons'>{intl.formatMessage({ id: 'topMenu.lessons' })}</Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link href='/comments'>{intl.formatMessage({ id: 'topMenu.comments' })}</Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a
+              href='https://www.facebook.com/Sloworz'
+              rel='external nofollow noreferrer'
+              target='_blank'
+            >
+              {intl.formatMessage({ id: 'topMenu.fb' })}
+            </a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a
+              href='https://www.instagram.com/polsko_kaszebsczi_sloworz'
+              rel='external nofollow noreferrer'
+              target='_blank'
+            >
+              {intl.formatMessage({ id: 'topMenu.instagram' })}
+            </a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a
+              href='https://twitter.com/kaszubskieslowa'
+              rel='external nofollow noreferrer'
+              target='_blank'
+            >
+              {intl.formatMessage({ id: 'topMenu.tweeter' })}
+            </a>
+          </MenuItem>
+        </Menu>
       </div>
     </header>
   );
