c9cd64d189fa32ec54b7d211446fb70a763c0bb1
JakubStefko <jakub.stefko@gmail.com>
2022-10-27 21:35:02 +0200

    ...

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['pl'], // , 'csb', 'en', 'de', 'uk'
+    defaultLocale: 'pl',
+    localeDetection: false,
+  },
+  env: {
+    API_URL: process.env.API_URL || 'https://kashubian-dic.herokuapp.com/',
+  },
+};
+
+module.exports = nextConfig;
