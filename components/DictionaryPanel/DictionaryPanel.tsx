1101ca2b53188e50f64a277fab923b0d628a8d31
JakubStefko <jakub.stefko@gmail.com>
2022-12-05 19:41:46 +0100

    ...

--- components/DictionaryPanel/DictionaryPanel.tsx
+++ components/DictionaryPanel/DictionaryPanel.tsx
@@ -1,8 +1,9 @@
+import { Input } from '@mui/material'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { useIntl } from 'react-intl'
-import { getWordList } from '../../utils/api'
+import { getWordListByString } from '../../utils/api'
 import errorHandler from '../../utils/errorHandler'
 import styles from './styles.module.css'
 
@@ -13,14 +14,20 @@ const DictionaryPanel = () => {
 	)
 	const [hasMore, setHasMore] = useState(true)
 	const [start, setStart] = useState(0)
+	const [searchBy, setSearchBy] = useState('')
 
-	async function loadMoreData() {
+	async function loadMoreData(
+		phrase = searchBy,
+		_start = start,
+		shouldWordListBeWiped = false
+	) {
 		try {
-			const response = await getWordList(50, start)
+			const response = await getWordListByString(phrase, 50, _start)
 			const _data = response.data?.data?.findAllKashubianEntries?.select || []
-			if (_data.length < 50) setHasMore(false)
-			setWordList([...wordList, ..._data])
-			setStart(start + 1)
+			setHasMore(_data.length > 49)
+			if (shouldWordListBeWiped) setWordList([..._data])
+			else setWordList([...wordList, ..._data])
+			setStart(_start + 1)
 		} catch (error) {
 			errorHandler(error, intl)
 		}
@@ -30,12 +37,26 @@ const DictionaryPanel = () => {
 		loadMoreData()
 	}, []) // eslint-disable-line
 
+	async function onInputChange(e: ChangeEvent<HTMLInputElement>) {
+		setSearchBy(e.target.value)
+		loadMoreData(e.target.value, 0, true)
+	}
+
 	return (
 		<article className={styles.dictionaryPanel}>
+			<Input
+				value={searchBy}
+				onChange={onInputChange}
+				placeholder={intl.formatMessage({ id: 'search' })}
+				sx={{
+					padding: '0.5rem',
+					fontSize: 20,
+				}}
+			/>
 			<InfiniteScroll
 				dataLength={wordList.length}
 				next={loadMoreData}
-				height={'calc(100vh - 160px)'}
+				height={'calc(100vh - 220px)'}
 				hasMore={hasMore}
 				loader={<h3>{intl.formatMessage({ id: 'loadingText' })}</h3>}
 				endMessage={<h4>{intl.formatMessage({ id: 'noMoreWords' })}</h4>}>
