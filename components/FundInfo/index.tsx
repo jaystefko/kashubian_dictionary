4b98fdfda259776d19340c162f8c0fff76f0210f
JakubStefko <jakub.stefko@gmail.com>
2022-12-21 21:10:31 +0100

    ...

--- components/FundInfo/index.tsx
+++ components/FundInfo/index.tsx
@@ -1,18 +1,23 @@
+import { useIntl } from 'react-intl'
 import styles from './styles.module.css'
 
 function FundInfo() {
+	const intl = useIntl()
+
 	return (
 		<section style={{ textAlign: 'center' }}>
 			<p className={styles.fundInfo}>
-				Tu powstaje słownik języka kaszubskiego. Możesz pomóc w jego powstaniu.
-				Wystarczy wejść na stronę:
+				<span>{intl.formatMessage({ id: 'main_page_text.first' })}</span>
 				<br />
+				<span>{intl.formatMessage({ id: 'main_page_text.second' })}</span>
 				<a
 					href='https://zrzutka.pl/nvtf5r'
 					rel='external nofollow noreferrer'
 					target='_blank'>
 					https://zrzutka.pl/nvtf5r
 				</a>
+				<br />
+				<span>{intl.formatMessage({ id: 'main_page_text.third' })}</span>
 			</p>
 		</section>
 	)
