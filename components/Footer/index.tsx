0b99e8938d8bfcbe09ffe3e9da794346f502d814
Jay <jay.stefko@gmail.com>
2025-10-17 22:13:31 +0200

    footer sponsor links updated

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,25 +7,55 @@ function Footer() {
 
 	return (
 		<footer className={styles.footer}>
-			<a
-				href='https://fundacjakaszuby.org/'
-				rel='external nofollow noreferrer'
-				className={`${styles.footerLink} ${styles.copyrightLink}`}
-				target='_blank'
-				title={intl.formatMessage({ id: 'sponsor.kashebianFoundation' })}>
-				<Image
-					priority={false}
-					src='/images/logo-kaszuby-foundation.png'
-					height={40}
-					width={133}
-					alt={intl.formatMessage({ id: 'sponsor.kashebianFoundation' })}
-					role='link'
-					title={intl.formatMessage({ id: 'sponsor.kashebianFoundation' })}
-				/>
-				<span className={styles.footerLinkText}>
-					© 2022 {intl.formatMessage({ id: 'sponsor.kashebianFoundation' })}
-				</span>
-			</a>
+			<section>
+				<ul className={styles.footerList}>
+					<li>
+						<a
+							href='https://fundacjakaszuby.org/'
+							rel='external nofollow noreferrer'
+							className={`${styles.footerLink} ${styles.copyrightLink}`}
+							target='_blank'
+							title={intl.formatMessage({ id: 'sponsor.kashebianFoundation' })}>
+							<Image
+								priority={false}
+								src='/images/logo-kaszuby-foundation.png'
+								height={40}
+								width={133}
+								alt={intl.formatMessage({ id: 'sponsor.kashebianFoundation' })}
+								role='link'
+								title={intl.formatMessage({
+									id: 'sponsor.kashebianFoundation',
+								})}
+							/>
+							<span className={styles.footerLinkText}>
+								© 2022{' '}
+								{intl.formatMessage({ id: 'sponsor.kashebianFoundation' })}
+							</span>
+						</a>
+					</li>
+					<li>
+						<a
+							href='https://www.muzeum.wejherowo.pl/'
+							rel='external nofollow noreferrer'
+							className={styles.footerLink}
+							target='_blank'
+							title={intl.formatMessage({ id: 'sponsor.museumWejherowo' })}>
+							<Image
+								priority={false}
+								src='/images/logo_MPiMK-P.svg'
+								height={40}
+								width={50}
+								alt={intl.formatMessage({ id: 'sponsor.museumWejherowo' })}
+								role='link'
+								title={intl.formatMessage({ id: 'sponsor.museumWejherowo' })}
+							/>
+							<span className={styles.footerLinkText}>
+								{intl.formatMessage({ id: 'sponsor.museumWejherowo' })}
+							</span>
+						</a>
+					</li>
+				</ul>
+			</section>
 			<section className={styles.sponsorContainer}>
 				<div className={styles.sponsorInfo}>
 					{intl.formatMessage({ id: 'sponsors' })}:
@@ -58,43 +88,85 @@ function Footer() {
 					</li>
 					<li>
 						<a
-							href='https://www.powiat.chojnice.pl/asp/Informacje%2CStrona_glowna%2C92'
+							href='https://szemud.pl'
 							rel='external nofollow noreferrer'
 							className={styles.footerLink}
 							target='_blank'
-							title={intl.formatMessage({ id: 'sponsor.countyChojnice' })}>
+							title={intl.formatMessage({ id: 'sponsor.szemud' })}>
 							<Image
 								priority={false}
-								src='/images/herb_powiat_chojnicki.svg'
+								src='/images/Szemud-cropped.svg'
 								height={40}
-								width={31}
-								alt={intl.formatMessage({ id: 'sponsor.countyChojnice' })}
+								width={70}
+								alt={intl.formatMessage({ id: 'sponsor.szemud' })}
 								role='link'
-								title={intl.formatMessage({ id: 'sponsor.countyChojnice' })}
+								title={intl.formatMessage({ id: 'sponsor.szemud' })}
 							/>
 							<span className={styles.footerLinkText}>
-								{intl.formatMessage({ id: 'sponsor.countyChojnice' })}
+								{intl.formatMessage({ id: 'sponsor.szemud' })}
 							</span>
 						</a>
 					</li>
 					<li>
 						<a
-							href='https://www.muzeum.wejherowo.pl/'
+							href='https://www.kartuzy.pl'
 							rel='external nofollow noreferrer'
 							className={styles.footerLink}
 							target='_blank'
-							title={intl.formatMessage({ id: 'sponsor.museumWejherowo' })}>
+							title={intl.formatMessage({ id: 'sponsor.kartuzy' })}>
 							<Image
 								priority={false}
-								src='/images/logo_MPiMK-P.svg'
+								src='/images/Kartuzy-cropped.svg'
 								height={40}
-								width={50}
-								alt={intl.formatMessage({ id: 'sponsor.museumWejherowo' })}
+								width={70}
+								alt={intl.formatMessage({ id: 'sponsor.kartuzy' })}
 								role='link'
-								title={intl.formatMessage({ id: 'sponsor.museumWejherowo' })}
+								title={intl.formatMessage({ id: 'sponsor.kartuzy' })}
 							/>
 							<span className={styles.footerLinkText}>
-								{intl.formatMessage({ id: 'sponsor.museumWejherowo' })}
+								{intl.formatMessage({ id: 'sponsor.kartuzy' })}
+							</span>
+						</a>
+					</li>
+					<li>
+						<a
+							href='https://www.dziemiany.pl'
+							rel='external nofollow noreferrer'
+							className={styles.footerLink}
+							target='_blank'
+							title={intl.formatMessage({ id: 'sponsor.dziemiany' })}>
+							<Image
+								priority={false}
+								src='/images/Dziemiany-cropped.svg'
+								height={40}
+								width={70}
+								alt={intl.formatMessage({ id: 'sponsor.dziemiany' })}
+								role='link'
+								title={intl.formatMessage({ id: 'sponsor.dziemiany' })}
+							/>
+							<span className={styles.footerLinkText}>
+								{intl.formatMessage({ id: 'sponsor.dziemiany' })}
+							</span>
+						</a>
+					</li>
+					<li>
+						<a
+							href='https://www.wladyslawowo.pl'
+							rel='external nofollow noreferrer'
+							className={styles.footerLink}
+							target='_blank'
+							title={intl.formatMessage({ id: 'sponsor.wladyslawowo' })}>
+							<Image
+								priority={false}
+								src='/images/Wladyslawowo-cropped.svg'
+								height={40}
+								width={70}
+								alt={intl.formatMessage({ id: 'sponsor.wladyslawowo' })}
+								role='link'
+								title={intl.formatMessage({ id: 'sponsor.wladyslawowo' })}
+							/>
+							<span className={styles.footerLinkText}>
+								{intl.formatMessage({ id: 'sponsor.wladyslawowo' })}
 							</span>
 						</a>
 					</li>
